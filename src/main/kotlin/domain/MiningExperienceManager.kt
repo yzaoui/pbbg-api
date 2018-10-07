436c4fcc4edb93a4c43c9d78180ff441117b0b0b
Yann Zaoui <yzaoui7@gmail.com>
2018-10-06 22:27:49 -0400

    Add mining levels

--- src/main/kotlin/domain/MiningExperienceManager.kt
+++ src/main/kotlin/domain/MiningExperienceManager.kt
@@ -4,7 +4,7 @@ import com.bitwiserain.pbbg.domain.model.LevelProgress
 import com.bitwiserain.pbbg.domain.model.LevelUp
 
 object MiningExperienceManager {
-    private val levels = listOf(20, 55, 85)
+    private val levels = listOf(20, 55, 85, 120, 160, 205, 255, 310, 370, 435, 505, 580)
 
     fun getLevelProgress(absoluteExp: Int): LevelProgress {
         var prevLevelAbsoluteExp = 0
