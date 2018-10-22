60f5b30618ad2e4b96f102c6723020ca4d5ccb8b
Yann Zaoui <yzaoui7@gmail.com>
2018-10-22 14:59:03 -0400

    Add form for changing password

--- src/main/kotlin/view/page/settingsPage.kt
+++ src/main/kotlin/view/page/settingsPage.kt
@@ -3,8 +3,52 @@ package com.bitwiserain.pbbg.view.page
 import com.bitwiserain.pbbg.view.template.MemberPageVM
 import com.bitwiserain.pbbg.view.template.MemberTemplate
 import io.ktor.html.Template
-import kotlinx.html.HTML
+import kotlinx.html.*
+
+const val currentPasswordId = "current-password"
+const val newPasswordId = "new-password"
+const val confirmNewPasswordId = "confirm-new-password"
 
 fun settingsPage(memberPageVM: MemberPageVM): Template<HTML> = MemberTemplate("Settings", memberPageVM).apply {
+    content {
+        form() {
+            fieldSet {
+                legend { +"Change password" }
 
+                label {
+                    htmlFor = currentPasswordId
+                    +"Current Password:"
+                }
+                input(type = InputType.password) {
+                    id = currentPasswordId
+                }
+
+                br { }
+
+                label {
+                    htmlFor = newPasswordId
+                    +"New Password:"
+                }
+                input(type = InputType.password) {
+                    id = newPasswordId
+                }
+
+                br { }
+
+                label {
+                    htmlFor = confirmNewPasswordId
+                    +"Confirm New Password:"
+                }
+                input(type = InputType.password) {
+                    id = confirmNewPasswordId
+                }
+
+                br { }
+
+                input(type = InputType.submit) {
+                    value = "Change"
+                }
+            }
+        }
+    }
 }
