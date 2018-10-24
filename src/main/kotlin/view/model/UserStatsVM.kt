149e5ce36385396577be96b46f5159e0b9ab2c72
Yann Zaoui <yzaoui7@gmail.com>
2018-10-24 14:50:22 -0400

    Remove mining level from home page

--- src/main/kotlin/view/model/UserStatsVM.kt
+++
@@ -1,6 +0,0 @@
-package com.bitwiserain.pbbg.view.model
-
-// TODO: Use JSON variant
-data class UserStatsVM(
-    val miningLevelProgress: LevelProgressVM
-)
--- src/main/kotlin/view/page/homeMemberPage.kt
+++ src/main/kotlin/view/page/homeMemberPage.kt
@@ -1,25 +1,9 @@
 package com.bitwiserain.pbbg.view.page
 
-import com.bitwiserain.pbbg.view.model.UserStatsVM
 import com.bitwiserain.pbbg.view.template.MemberPageVM
 import com.bitwiserain.pbbg.view.template.MemberTemplate
 import io.ktor.html.Template
 import kotlinx.html.*
 
-fun homeMemberPage(userStatsVM: UserStatsVM, memberPageVM: MemberPageVM): Template<HTML> = MemberTemplate("Home", memberPageVM).apply {
-    content {
-        div {
-            span {
-                +"Mining Level ${userStatsVM.miningLevelProgress.level}: "
-            }
-            meter {
-                value = userStatsVM.miningLevelProgress.relativeExp.toString()
-                min = 0.toString()
-                max = userStatsVM.miningLevelProgress.relativeExpNextLevel.toString()
-            }
-            span {
-                +" ${userStatsVM.miningLevelProgress.relativeExp} / ${userStatsVM.miningLevelProgress.relativeExpNextLevel} Exp."
-            }
-        }
-    }
+fun homeMemberPage(memberPageVM: MemberPageVM): Template<HTML> = MemberTemplate("Home", memberPageVM).apply {
 }
