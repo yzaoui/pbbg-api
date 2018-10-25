7a9e856774344a033ff5a6af4cd547e6bd53d5a4
Yann Zaoui <yzaoui7@gmail.com>
2018-10-25 14:40:10 -0400

    Add WIP indicator on squad & battle pages

--- src/main/kotlin/view/page/battlePage.kt
+++ src/main/kotlin/view/page/battlePage.kt
@@ -4,7 +4,10 @@ import com.bitwiserain.pbbg.view.template.MemberPageVM
 import com.bitwiserain.pbbg.view.template.MemberTemplate
 import io.ktor.html.Template
 import kotlinx.html.HTML
+import kotlinx.html.p
 
 fun battlePage(memberPageVM: MemberPageVM): Template<HTML> = MemberTemplate("Battle", memberPageVM).apply {
-
+    content {
+        p { +"Work in Progress." }
+    }
 }
--- src/main/kotlin/view/page/squadPage.kt
+++ src/main/kotlin/view/page/squadPage.kt
@@ -4,7 +4,10 @@ import com.bitwiserain.pbbg.view.template.MemberPageVM
 import com.bitwiserain.pbbg.view.template.MemberTemplate
 import io.ktor.html.Template
 import kotlinx.html.HTML
+import kotlinx.html.p
 
 fun squadPage(memberPageVM: MemberPageVM): Template<HTML> = MemberTemplate("Squad", memberPageVM).apply {
-
+    content {
+        p { +"Work in Progress." }
+    }
 }
