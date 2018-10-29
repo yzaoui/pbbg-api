3cfdf21b2d9d42a74893e0c41f6d4e6187a45eba
Yann Zaoui <yzaoui7@gmail.com>
2018-10-29 12:18:43 -0400

    Add attack API route

--- src/main/kotlin/route/api/battleAPI.kt
+++ src/main/kotlin/route/api/battleAPI.kt
@@ -3,6 +3,7 @@ package com.bitwiserain.pbbg.route.api
 import com.bitwiserain.pbbg.*
 import com.bitwiserain.pbbg.domain.usecase.UserUC
 import io.ktor.application.call
+import io.ktor.request.receive
 import io.ktor.routing.*
 
 fun Route.battleAPI(userUC: UserUC, battleUC: BattleUC) = route("/battle") {
@@ -38,9 +39,23 @@ fun Route.battleAPI(userUC: UserUC, battleUC: BattleUC) = route("/battle") {
             }
         }
     }
+
+    route("/attack") {
+        post {
+            val loggedInUser = call.attributes[loggedInUserKey]
+
+            val params = call.receive<AttackParams>()
+
+            battleUC.attack(loggedInUser.id, allyId = params.allyId, enemyId = params.enemyId)
+
+            call.respondSuccess()
+        }
+    }
 }
 
 fun Battle.toJSON() = BattleJSON(
     allies = allies.map { it.toJSON() },
     enemies = enemies.map { it.toJSON() }
 )
+
+class AttackParams(val allyId: Long, val enemyId: Long)
