10fb4f30338bfee4f502950be58bd1341f26d7bb
Yann Zaoui <yzaoui7@gmail.com>
2018-11-01 16:31:13 -0400

    Add experience bar to client

--- src/main/resources/js/component/pbbg-unit.js
+++ src/main/resources/js/component/pbbg-unit.js
@@ -1,9 +1,17 @@
 /**
+ * @typedef {Object} LevelProgress
+ * @property {number} level - Current level.
+ * @property {number} relativeExp - Experience since the start of this level.
+ * @property {number} relativeExpToNextLevel - Experience required to level up since the start of this level.
+ */
+
+/**
  * @typedef {Object} Unit
  * @property {string} name - Unit name.
  * @property {number} hp - Current HP.
  * @property {number} maxHP - Maximum HP.
  * @property {number} atk - Current ATK.
+ * @property {LevelProgress} levelProgress - Unit's level and experience information.
  */
 
 class PBBGUnit extends HTMLElement {
@@ -18,7 +26,7 @@ class PBBGUnit extends HTMLElement {
 :host {
     display: inline-block;
     width: 18em;
-    height: 5em;
+    height: 5.3em;
     box-sizing: border-box;
     border: 1px solid #333333;
     padding: 6px;
@@ -32,6 +40,11 @@ class PBBGUnit extends HTMLElement {
     width: 120px;
     height: 11px;
 }
+
+#exp-bar {
+    width: 160px;
+    height: 8px;
+}
 </style>
 <div>
     <span id="name"></span>
@@ -43,6 +56,11 @@ class PBBGUnit extends HTMLElement {
 </div>
 <div>
     <span>ATK: <span id="atk-value"></span></span>
+</div>
+<div>
+    <span>Level <span id="level-value"></span></span>
+    <pbbg-progress-bar id="exp-bar"></pbbg-progress-bar>
+    <span id="exp-value"></span>
 </div>
 `;
 
@@ -81,6 +99,10 @@ class PBBGUnit extends HTMLElement {
         this._shadowRoot.getElementById("hp-bar").max = this._unit.maxHP;
         this._shadowRoot.getElementById("hp-value").innerText = `${this._unit.hp} / ${this._unit.maxHP}`;
         this._shadowRoot.getElementById("atk-value").innerText = this._unit.atk.toString();
+        this._shadowRoot.getElementById("level-value").innerText = this._unit.levelProgress.level.toString();
+        this._shadowRoot.getElementById("exp-bar").value = this._unit.levelProgress.relativeExp;
+        this._shadowRoot.getElementById("exp-bar").max = this._unit.levelProgress.relativeExpToNextLevel;
+        this._shadowRoot.getElementById("exp-value").innerText = `${this._unit.levelProgress.relativeExp} / ${this._unit.levelProgress.relativeExpToNextLevel}`;
     }
 }
 
