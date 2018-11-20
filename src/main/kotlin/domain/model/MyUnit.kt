59471e7934bc348f67cb81f042040a4ca3ea770f
Yann Zaoui <yzaoui7@gmail.com>
2018-11-19 23:13:25 -0500

    Add support for unit sprites to backend

--- src/main/kotlin/domain/model/MyUnit.kt
+++ src/main/kotlin/domain/model/MyUnit.kt
@@ -7,6 +7,7 @@ sealed class MyUnit {
     abstract val id: Long
     abstract val name: String
     abstract val enum: MyUnitEnum
+    abstract val spriteName: String
     abstract val hp: Int
     abstract val maxHP: Int
     abstract val atk: Int
@@ -18,24 +19,27 @@ sealed class MyUnit {
         override val id: Long, override val hp: Int, override val maxHP: Int, override val atk: Int,
         override val exp: Long
     ) : MyUnit() {
-        override val name: String get() = "Ice-Cream Wizard"
+        override val name get() = "Ice-Cream Wizard"
         override val enum get() = ICE_CREAM_WIZARD
+        override val spriteName get() = "ice-cream-wizard"
     }
 
     data class Twolip(
         override val id: Long, override val hp: Int, override val maxHP: Int, override val atk: Int,
         override val exp: Long
     ) : MyUnit() {
-        override val name: String get() = "Twolip"
+        override val name get() = "Twolip"
         override val enum get() = TWOLIP
+        override val spriteName get() = "twolip"
     }
 
     data class Carpshooter(
         override val id: Long, override val hp: Int, override val maxHP: Int, override val atk: Int,
         override val exp: Long
     ) : MyUnit() {
-        override val name: String get() = "Carpshooter"
+        override val name get() = "Carpshooter"
         override val enum get() = CARPSHOOTER
+        override val spriteName get() = "carpshooter"
     }
 
     fun receiveDamage(damage: Int): MyUnit {
