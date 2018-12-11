0d390d3f4e3e9840e7c1d458122e655ed265984e
Yann Zaoui <yzaoui7@gmail.com>
2018-12-11 16:35:19 -0500

    Refactor home.js

--- src/main/kotlin/view/page/homeMemberPage.kt
+++ src/main/kotlin/view/page/homeMemberPage.kt
@@ -7,8 +7,6 @@ import kotlinx.html.*
 
 fun homeMemberPage(memberPageVM: MemberPageVM): Template<HTML> = MemberTemplate("Home", memberPageVM).apply {
     endOfBody {
-        script(src = "/js/webcomponents-bundle-2.0.0.js") {}
-        script(src = "/js/component/pbbg-progress-bar.js") {}
         script(src = "/js/home.js") {}
     }
 }
