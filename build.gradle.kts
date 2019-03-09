050ed2ef15d73a6becf0d8fe624d1166fb73eb29
Yann Zaoui <yzaoui7@gmail.com>
2019-03-08 21:36:21 -0500

    Add PostgreSQL driver dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ repositories {
 
 dependencies {
     compile("com.h2database:h2:1.4.197")
+    compile(group = "org.postgresql", name = "postgresql", version = "42.2.1")
     compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.KOTLIN}")
     compile("org.jetbrains.exposed:exposed:0.12.2")
     compile("at.favre.lib:bcrypt:0.7.0")
