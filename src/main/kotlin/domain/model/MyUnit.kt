174b3d6b92d1b9c5ffef3b28bd9d33fff77d8b5a
Yann Zaoui <yzaoui7@gmail.com>
2019-03-21 11:28:10 -0400

    Move static unit properties to enum

--- src/main/kotlin/domain/model/MyUnit.kt
+++ src/main/kotlin/domain/model/MyUnit.kt
@@ -5,9 +5,7 @@ import kotlin.math.max
 
 sealed class MyUnit {
     abstract val id: Long
-    abstract val name: String
     abstract val enum: MyUnitEnum
-    abstract val spriteName: String
     abstract val hp: Int
     abstract val maxHP: Int
     abstract val atk: Int
@@ -19,27 +17,21 @@ sealed class MyUnit {
         override val id: Long, override val hp: Int, override val maxHP: Int, override val atk: Int,
         override val exp: Long
     ) : MyUnit() {
-        override val name get() = "Ice-Cream Wizard"
         override val enum get() = ICE_CREAM_WIZARD
-        override val spriteName get() = "ice-cream-wizard"
     }
 
     data class Twolip(
         override val id: Long, override val hp: Int, override val maxHP: Int, override val atk: Int,
         override val exp: Long
     ) : MyUnit() {
-        override val name get() = "Twolip"
         override val enum get() = TWOLIP
-        override val spriteName get() = "twolip"
     }
 
     data class Carpshooter(
         override val id: Long, override val hp: Int, override val maxHP: Int, override val atk: Int,
         override val exp: Long
     ) : MyUnit() {
-        override val name get() = "Carpshooter"
         override val enum get() = CARPSHOOTER
-        override val spriteName get() = "carpshooter"
     }
 
     fun receiveDamage(damage: Int): MyUnit {
--- src/main/kotlin/domain/model/MyUnitEnum.kt
+++ src/main/kotlin/domain/model/MyUnitEnum.kt
@@ -1,7 +1,7 @@
 package com.bitwiserain.pbbg.domain.model
 
-enum class MyUnitEnum {
-    ICE_CREAM_WIZARD,
-    TWOLIP,
-    CARPSHOOTER
+enum class MyUnitEnum(val friendlyName: String, val spriteName: String) {
+    ICE_CREAM_WIZARD("Ice-Cream Wizard", "ice-cream-wizard"),
+    TWOLIP("Twolip", "twolip"),
+    CARPSHOOTER("Carpshooter", "carpshooter")
 }
