c1f7130a937cc157397da8bc2c15f48d6af9e7d7
Yann Zaoui <yzaoui7@gmail.com>
2019-05-04 11:13:11 -0400

    Add kotlinx.serialization dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ plugins {
     application
     kotlin("jvm").version(/*Versions.KOTLIN*/"1.3.30")
     id("com.github.johnrengelman.shadow").version("5.0.0")
+    id("kotlinx-serialization").version(/*Versions.KOTLIN*/"1.3.30")
 }
 
 application {
@@ -24,6 +25,7 @@ repositories {
     mavenCentral()
     jcenter()
     maven { url = URI("https://kotlin.bintray.com/ktor") }
+    maven { url = URI("https://kotlin.bintray.com/kotlinx") }
     maven { url = URI("https://dl.bintray.com/kotlin/exposed") }
 }
 
@@ -31,6 +33,7 @@ dependencies {
     implementation(group = "com.h2database", name = "h2", version = "1.4.197")
     implementation(group = "org.postgresql", name = "postgresql", version = "42.2.1")
     implementation(group = "org.jetbrains.kotlin", name = "kotlin-stdlib-jdk8", version = Versions.KOTLIN)
+    implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-serialization-runtime", version = "0.11.0")
     implementation(group = "org.jetbrains.exposed", name = "exposed", version = "0.12.2")
     implementation(group = "at.favre.lib", name = "bcrypt", version = "0.7.0")
     implementation(group = "io.ktor", name = "ktor-locations", version = Versions.KTOR)
