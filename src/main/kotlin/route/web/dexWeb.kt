2c0f608c5c372233292a3502438e1a21c3b7fdff
Yann Zaoui <yzaoui7@gmail.com>
2019-05-28 13:43:39 -0400

    Allow specific dex unit URL on web path

--- src/main/kotlin/route/web/dexWeb.kt
+++ src/main/kotlin/route/web/dexWeb.kt
@@ -1,5 +1,6 @@
 package com.bitwiserain.pbbg.route.web
 
+import com.bitwiserain.pbbg.domain.model.MyUnitEnum
 import com.bitwiserain.pbbg.domain.usecase.UserUC
 import com.bitwiserain.pbbg.interceptSetUserOrRedirect
 import com.bitwiserain.pbbg.memberPageVM
@@ -17,18 +18,28 @@ import io.ktor.routing.route
 @Location("/dex")
 class DexWebLocation
 
-fun Route.dexWeb(userUC: UserUC) = route("/dex/{path...}") {
+const val PATH = "path"
+
+fun Route.dexWeb(userUC: UserUC) = route("/dex/{$PATH...}") {
+    interceptSetUserOrRedirect(userUC)
+
+    /**
+     * Responds with 404 if route is not one of:
+     *   /dex
+     *   /dex/items
+     *   /dex/units
+     *   /dex/units/{valid dex unit ID}
+     */
     intercept(ApplicationCallPipeline.Features) {
-        when (call.parameters.getAll("path")?.joinToString("/")) {
-            "", "items", "units" -> Unit
-            else -> {
-                call.respond(HttpStatusCode.NotFound)
-                finish()
-            }
+        val path = call.parameters.getAll(PATH)!!
+
+        if (!(path.joinToString("/") in listOf("", "items", "units")
+                    || (path.size == 2 && path[0] == "units" && path[1].toIntOrNull() in MyUnitEnum.values().indices))
+        ) {
+            call.respond(HttpStatusCode.NotFound)
+            finish()
         }
     }
-
-    interceptSetUserOrRedirect(userUC)
 
     get {
         call.respondHtmlTemplate(dexPage(memberPageVM = call.attributes[memberPageVM])) {}
