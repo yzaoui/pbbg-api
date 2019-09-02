20b07431c2b45bd9098befd6e4ae2f9c421f1fc7
Yann Zaoui <yzaoui7@gmail.com>
2019-09-02 16:07:21 -0400

    Add gold field to user stats table

--- src/main/kotlin/db/repository/UserStatsTable.kt
+++ src/main/kotlin/db/repository/UserStatsTable.kt
@@ -4,5 +4,6 @@ import org.jetbrains.exposed.sql.Table
 
 object UserStatsTable : Table() {
     val userId = reference("user_id", UserTable)
+    val gold = long("gold").default(0)
     val miningExp = long("mining_exp").default(0)
 }
