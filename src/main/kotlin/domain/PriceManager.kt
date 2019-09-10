18555fa7876ec854d5a0361528a832458f38b451
Yann Zaoui <yzaoui7@gmail.com>
2019-09-09 21:16:44 -0400

    Adjust market prices

--- src/main/kotlin/domain/PriceManager.kt
+++ src/main/kotlin/domain/PriceManager.kt
@@ -1,13 +1,18 @@
 package com.bitwiserain.pbbg.domain
 
 import com.bitwiserain.pbbg.domain.model.MaterializedItem
+import com.bitwiserain.pbbg.domain.model.MaterializedItem.*
 
 object PriceManager {
-    fun getSellPrice(item: MaterializedItem): Int {
-        return 3
+    fun getSellPrice(item: MaterializedItem): Int = when (item) {
+        is Stone, is Coal, is CopperOre -> 3
+        is IcePick -> 5
+        is PlusPickaxe, is CrossPickaxe, is SquarePickaxe -> 7
     }
 
-    fun getBuyPrice(item: MaterializedItem): Int {
-        return 7
+    fun getBuyPrice(item: MaterializedItem): Int = when (item) {
+        is Stone, is Coal, is CopperOre -> 10
+        is IcePick -> 15
+        is PlusPickaxe, is CrossPickaxe, is SquarePickaxe -> 20
     }
 }
