da342f5e0bbe11ab6c1938ac65cfeec3eda35a06
Yann Zaoui <yzaoui7@gmail.com>
2019-10-31 00:36:26 -0400

    Remove hard-coded item history

--- src/main/kotlin/db/usecase/Common.kt
+++ src/main/kotlin/db/usecase/Common.kt
@@ -1,13 +1,14 @@
 package com.bitwiserain.pbbg.db.usecase
 
-import com.bitwiserain.pbbg.db.repository.DexTable
-import com.bitwiserain.pbbg.db.repository.InventoryTable
-import com.bitwiserain.pbbg.db.repository.Joins
-import com.bitwiserain.pbbg.db.repository.MaterializedItemTable
+import com.bitwiserain.pbbg.db.repository.*
 import com.bitwiserain.pbbg.domain.model.MaterializedItem
+import com.bitwiserain.pbbg.domain.model.itemdetails.ItemHistory
+import com.bitwiserain.pbbg.domain.model.itemdetails.ItemHistoryInfo
 import org.jetbrains.exposed.dao.EntityID
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.time.Instant
+import java.time.temporal.ChronoUnit
 
 fun storeInInventoryReturnItemID(db: Database, userId: EntityID<Int>, itemToStore: MaterializedItem): EntityID<Long> = transaction(db) {
     val heldItems = Joins.getHeldItemsOfBaseKind(userId, itemToStore.enum)
@@ -16,18 +17,22 @@ fun storeInInventoryReturnItemID(db: Database, userId: EntityID<Int>, itemToStor
 
     if (heldItems.count() == 1 && heldItems.values.single() is MaterializedItem.Stackable) {
         itemToStore as MaterializedItem.Stackable
-        // If this kind of item is already stored, and it can be stacked, increase its quantity
+        // If this item is currently held and Stackable, increase its quantity
         itemId = heldItems.keys.single()
         MaterializedItemTable.updateQuantity(heldItems.keys.single(), itemToStore.quantity)
     } else {
-        // If this item isn't already stored, or it can't be stacked, create a new row for it
+        // If this item isn't already stored, or it can't be stacked, create a new entry for it
         itemId = MaterializedItemTable.insertItemAndGetId(itemToStore)
         InventoryTable.insertItem(userId, itemId, itemToStore.base)
-    }
 
-    /*************************************************
-     * Update user's dex to check off this item type *
-     *************************************************/
+        if (heldItems.count() == 0) {
+            // TODO: For now, assume only stackable items are MineEntity
+            ItemHistoryTable.insertItemHistory(itemId.value, ItemHistory(
+                date = Instant.now().truncatedTo(ChronoUnit.SECONDS),
+                info = ItemHistoryInfo.FirstMined(userId.value)
+            ))
+        }
+    }
 
     /*************************************************
      * Update user's dex to check off this item type *
--- src/main/kotlin/db/usecase/ItemUCImpl.kt
+++ src/main/kotlin/db/usecase/ItemUCImpl.kt
@@ -4,13 +4,10 @@ import com.bitwiserain.pbbg.db.repository.ItemHistoryTable
 import com.bitwiserain.pbbg.db.repository.MaterializedItemTable
 import com.bitwiserain.pbbg.db.repository.UserTable
 import com.bitwiserain.pbbg.domain.model.itemdetails.ItemDetails
-import com.bitwiserain.pbbg.domain.model.itemdetails.ItemHistory
-import com.bitwiserain.pbbg.domain.model.itemdetails.ItemHistoryInfo
 import com.bitwiserain.pbbg.domain.usecase.ItemNotFoundException
 import com.bitwiserain.pbbg.domain.usecase.ItemUC
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.transaction
-import java.time.Instant
 
 class ItemUCImpl(private val db: Database) : ItemUC {
     override fun getItemDetails(itemId: Long): ItemDetails = transaction(db) {
@@ -18,15 +15,12 @@ class ItemUCImpl(private val db: Database) : ItemUC {
 
         val itemHistory = ItemHistoryTable.getItemHistoryList(itemId)
 
+        // TODO: Currently hardcoding user 1
         val userInfo = UserTable.getUserById(1)!!.let { mapOf(it.id to it.username) }
 
         return@transaction ItemDetails(
             item = item,
-            history = listOf(
-                ItemHistory(Instant.now().minusSeconds(60 * 60 * 1), ItemHistoryInfo.FirstMined(1)),
-                ItemHistory(Instant.now().minusSeconds(60 * 60 * 3), ItemHistoryInfo.CreatedWithUser(1)),
-                ItemHistory(Instant.now().minusSeconds(60 * 60 * 2), ItemHistoryInfo.CreatedInMarket())
-            ),
+            history = itemHistory,
             linkedUserInfo = userInfo
         )
     }
