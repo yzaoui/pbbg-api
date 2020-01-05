4e978f5dc3f89fd25e834713957795ab2905b8b4
Yann Zaoui <contact@yannzaoui.com>
2020-01-05 17:27:30 -0500

    Update inventory API to allow filtering plantable items

--- src/main/kotlin/db/usecase/InventoryUCImpl.kt
+++ src/main/kotlin/db/usecase/InventoryUCImpl.kt
@@ -2,19 +2,22 @@ package com.bitwiserain.pbbg.db.usecase
 
 import com.bitwiserain.pbbg.db.repository.Joins
 import com.bitwiserain.pbbg.db.repository.UserTable
+import com.bitwiserain.pbbg.domain.model.BaseItem
 import com.bitwiserain.pbbg.domain.model.Inventory
+import com.bitwiserain.pbbg.domain.usecase.InventoryFilter
 import com.bitwiserain.pbbg.domain.usecase.InventoryUC
 import org.jetbrains.exposed.dao.EntityID
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class InventoryUCImpl(private val db: Database) : InventoryUC {
-    override fun getInventory(userId: Int): Inventory = transaction(db) {
-        // TODO: Consider checking if user exists
-        val userId = EntityID(userId, UserTable)
-
-        val items = Joins.getInventoryItems(userId)
-
-        Inventory(items)
+    override fun getInventory(userId: Int, filter: InventoryFilter?): Inventory = transaction(db) {
+        return@transaction Joins.getInventoryItems(EntityID(userId, UserTable))
+            .run { if (filter != null) filter { it.value.base.matchesInventoryFilter(filter) } else this }
+            .let { Inventory(it) }
     }
+}
+
+private fun BaseItem.matchesInventoryFilter(filter: InventoryFilter): Boolean = when (filter) {
+    InventoryFilter.PLANTABLE -> this is BaseItem.Plantable
 }
--- src/main/kotlin/domain/usecase/InventoryUC.kt
+++ src/main/kotlin/domain/usecase/InventoryUC.kt
@@ -3,5 +3,9 @@ package com.bitwiserain.pbbg.domain.usecase
 import com.bitwiserain.pbbg.domain.model.Inventory
 
 interface InventoryUC {
-    fun getInventory(userId: Int): Inventory
+    fun getInventory(userId: Int, filter: InventoryFilter? = null): Inventory
+}
+
+enum class InventoryFilter {
+    PLANTABLE
 }
