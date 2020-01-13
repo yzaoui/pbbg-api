a72ab32172c4e7aa9a2e2be1544423e8493b3661
Yann Zaoui <contact@yannzaoui.com>
2020-01-13 01:39:25 -0500

    Add serialized name annotations to plant JSON entities

--- src/main/kotlin/view/model/farm/BasePlantJSON.kt
+++ src/main/kotlin/view/model/farm/BasePlantJSON.kt
@@ -1,8 +1,10 @@
 package com.bitwiserain.pbbg.view.model.farm
 
+import com.google.gson.annotations.SerializedName
+
 data class BasePlantJSON(
-    val growingPeriod: Long,
-    val growingSprite: String,
-    val maturePeriod: Long?,
-    val matureSprite: String?
+    @SerializedName("growingPeriod") val growingPeriod: Long,
+    @SerializedName("growingSprite") val growingSprite: String,
+    @SerializedName("maturePeriod") val maturePeriod: Long?,
+    @SerializedName("matureSprite") val matureSprite: String?
 )
--- src/main/kotlin/view/model/farm/MaterializedPlantJSON.kt
+++ src/main/kotlin/view/model/farm/MaterializedPlantJSON.kt
@@ -1,7 +1,9 @@
 package com.bitwiserain.pbbg.view.model.farm
 
+import com.google.gson.annotations.SerializedName
+
 data class MaterializedPlantJSON(
-    val basePlant: BasePlantJSON,
-    val cycleStart: String,
-    val isMature: Boolean?
+    @SerializedName("basePlant") val basePlant: BasePlantJSON,
+    @SerializedName("cycleStart") val cycleStart: String,
+    @SerializedName("isMature") val isMature: Boolean?
 )
