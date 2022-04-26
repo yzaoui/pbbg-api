069e4becb087fa77fe7ae6d2e759e0446f0ec25a
Yann Zaoui <contact@yannzaoui.com>
2022-04-25 23:25:45 -0400

    Create testIntegration source set, Gradle configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,7 @@ object Versions {
     const val KTOR = "1.6.4"
     const val EXPOSED = "0.37.3"
     const val JUNIT_JUPITER = "5.8.1"
+    const val KOTEST = "5.3.0"
 }
 
 plugins {
@@ -23,6 +24,24 @@ application {
     mainClass.set("io.ktor.server.netty.EngineMain")
 }
 
+sourceSets {
+    create("testIntegration") {
+        compileClasspath += sourceSets.main.get().output
+        runtimeClasspath += sourceSets.main.get().output
+    }
+}
+
+val testIntegrationImplementation: Configuration by configurations.getting {
+    extendsFrom(configurations.implementation.get())
+}
+
+val testIntegrationRuntimeOnly: Configuration by configurations.getting {
+    extendsFrom(configurations.runtimeOnly.get())
+}
+
+configurations["testIntegrationImplementation"].extendsFrom(configurations.implementation.get())
+configurations["testIntegrationRuntimeOnly"].extendsFrom(configurations.runtimeOnly.get())
+
 dependencies {
     implementation(group = "com.h2database", name = "h2", version = "2.1.210")
     implementation(group = "org.postgresql", name = "postgresql", version = "42.2.18")
@@ -36,18 +55,40 @@ dependencies {
     implementation(group = "ch.qos.logback", name = "logback-classic", version = "1.2.3")
 
     testImplementation(kotlin("test"))
-    testImplementation(group = "io.ktor", name = "ktor-server-test-host", version = Versions.KTOR)
-    testImplementation(group = "io.kotest", name = "kotest-assertions-core", version = "5.3.0")
-    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = Versions.JUNIT_JUPITER)
+    testImplementation(group = "io.kotest", name = "kotest-assertions-core", version = Versions.KOTEST)
     testImplementation(group = "io.mockk", name = "mockk", version = "1.12.3")
+    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = Versions.JUNIT_JUPITER)
     testRuntimeOnly(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = Versions.JUNIT_JUPITER)
+
+    testIntegrationImplementation(kotlin("test"))
+    testIntegrationImplementation(group = "io.ktor", name = "ktor-server-test-host", version = Versions.KTOR)
+    testIntegrationImplementation(group = "io.kotest", name = "kotest-assertions-core", version = Versions.KOTEST)
+    testIntegrationImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = Versions.JUNIT_JUPITER)
+    testIntegrationRuntimeOnly(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = Versions.JUNIT_JUPITER)
 }
 
 tasks.withType<ShadowJar> {
     archiveClassifier.set("")
 }
 
-idea.module.settings {
-    packagePrefix["src/main/kotlin"] = "com.bitwiserain.pbbg.app"
-    packagePrefix["src/test/kotlin"] = "com.bitwiserain.pbbg.app.test"
+val testIntegration = task<Test>("testIntegration") {
+    description = "Runs integration tests."
+    group = "verification"
+
+    testClassesDirs = sourceSets["testIntegration"].output.classesDirs
+    classpath = sourceSets["testIntegration"].runtimeClasspath
+    shouldRunAfter("test")
+}
+
+tasks.test {
+    dependsOn(testIntegration)
+}
+
+idea.module {
+    testSourceDirs.addAll(project.sourceSets["testIntegration"].allSource.srcDirs)
+    settings {
+        packagePrefix["src/main/kotlin"] = "com.bitwiserain.pbbg.app"
+        packagePrefix["src/test/kotlin"] = "com.bitwiserain.pbbg.app.test"
+        packagePrefix["src/testIntegration/kotlin"] = "com.bitwiserain.pbbg.app.testintegration"
+    }
 }
