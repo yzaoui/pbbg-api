c7c989060f84a8710a31c8806f9ca9a5f81efead
Yann Zaoui <contact@yannzaoui.com>
2022-06-05 13:20:47 -0400

    Replace memory database with mocks in ChangePasswordUCImplTest

--- app/src/test/kotlin/domain/usecase/ChangePasswordUCImplTest.kt
+++ app/src/test/kotlin/domain/usecase/ChangePasswordUCImplTest.kt
@@ -2,114 +2,144 @@ package com.bitwiserain.pbbg.app.test.domain.usecase
 
 import com.bitwiserain.pbbg.app.BCryptHelper
 import com.bitwiserain.pbbg.app.PASSWORD_REGEX
-import com.bitwiserain.pbbg.app.SchemaHelper
-import com.bitwiserain.pbbg.app.db.repository.UserTableImpl
+import com.bitwiserain.pbbg.app.db.model.User
+import com.bitwiserain.pbbg.app.db.repository.UserTable
 import com.bitwiserain.pbbg.app.domain.usecase.ChangePasswordUC.Result
 import com.bitwiserain.pbbg.app.domain.usecase.ChangePasswordUCImpl
-import com.bitwiserain.pbbg.app.test.createTestUserAndGetId
-import com.bitwiserain.pbbg.app.test.initDatabase
-import io.kotest.matchers.booleans.shouldBeFalse
-import io.kotest.matchers.booleans.shouldBeTrue
-import io.kotest.matchers.nulls.shouldNotBeNull
+import com.bitwiserain.pbbg.app.test.db.TestTransaction
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeTypeOf
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkObject
+import io.mockk.unmockkObject
+import io.mockk.verify
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.time.Instant
 
 class ChangePasswordUCImplTest {
 
-    private val transaction = initDatabase()
-    private val userTable = UserTableImpl()
-    private val changePassword = ChangePasswordUCImpl(transaction, userTable)
+    private val userId = 1234
+
+    private val userTable: UserTable = mockk(relaxUnitFun = true)
+
+    private val changePassword: ChangePasswordUCImpl = ChangePasswordUCImpl(TestTransaction, userTable)
+
+    @BeforeEach
+    fun setUp() {
+        mockkObject(BCryptHelper)
+    }
 
     @AfterEach
-    fun dropDatabase() {
-        SchemaHelper.dropTables(transaction)
+    fun tearDown() {
+        unmockkObject(BCryptHelper)
     }
 
     @Test
     fun `Given an existing user, when changing password with correct parameters, password should be changed`() {
-        val userId = createTestUserAndGetId(transaction, userTable, "user", "pass123")
+        val currentPassword = "currentPassword"
+        val currentPasswordHash = byteArrayOf(1, 2, 3, 4)
+        val newPassword = "newPassword"
+        val newPasswordHash = byteArrayOf(5, 6, 7, 8)
+        val confirmNewPassword = "newPassword"
+        assert(newPassword == confirmNewPassword )
+        assert(newPassword.matches(PASSWORD_REGEX.toRegex()))
 
-        val newPassword = "new27pas".also { it.matches(PASSWORD_REGEX.toRegex()).shouldBeTrue() }
+        every { userTable.getUserById(userId) } returns User(
+            id = userId,
+            username = "user",
+            passwordHash = currentPasswordHash,
+            joinedInstant = Instant.now()
+        )
+        every { BCryptHelper.verifyPassword(currentPassword, currentPasswordHash) } returns true
+        every { BCryptHelper.hashPassword(newPassword) } returns newPasswordHash
 
-        changePassword(userId, currentPassword = "pass123", newPassword = newPassword, confirmNewPassword = newPassword)
-            .shouldBeTypeOf<Result.Success>()
 
-        val changedUser = transaction { userTable.getUserById(userId) }
+        val result = changePassword(userId, currentPassword, newPassword, confirmNewPassword)
 
-        changedUser.shouldNotBeNull()
-        changedUser.username shouldBe "user"
-        BCryptHelper.verifyPassword(newPassword, changedUser.passwordHash).shouldBeTrue()
+        verify { userTable.updatePassword(userId, newPasswordHash) }
+
+        result shouldBe Result.Success
     }
 
     @Test
     fun `Given an existing user, when changing password with an incorrect current password parameter, WrongCurrentPasswordError should be returned`() {
-        val originalPassword = "pass123".also { it.matches(PASSWORD_REGEX.toRegex()).shouldBeTrue() }
-        val userId = createTestUserAndGetId(transaction, userTable, "usr71", originalPassword)
+        val actualCurrentPasswordHash = byteArrayOf(1, 2, 3, 4)
+        val attemptedCurrentPassword = "inc0rrect4"
+        val newPassword = "newPassword"
 
-        val newPassword = "5fkd^s91$-".also { it.matches(PASSWORD_REGEX.toRegex()).shouldBeTrue() }
+        every { userTable.getUserById(userId) } returns User(
+            id = userId,
+            username = "user",
+            passwordHash = actualCurrentPasswordHash,
+            joinedInstant = Instant.now()
+        )
+        // Current password fails to verify
+        every { BCryptHelper.verifyPassword(attemptedCurrentPassword, actualCurrentPasswordHash) } returns false
 
-        changePassword(userId, "inc0rrect4", newPassword, newPassword)
+        changePassword(userId, attemptedCurrentPassword, newPassword, newPassword)
             .shouldBeTypeOf<Result.WrongCurrentPasswordError>()
-
-        val latestUser = transaction { userTable.getUserById(userId) }
-
-        latestUser.shouldNotBeNull()
-        latestUser.username shouldBe "usr71"
-        BCryptHelper.verifyPassword(originalPassword, latestUser.passwordHash).shouldBeTrue()
     }
 
     @Test
     fun `Given an existing user, when changing password with an incorrectly confirmed new password parameter, UnconfirmedNewPasswordError should be returned`() {
-        val originalPassword = "pass123".also { it.matches(PASSWORD_REGEX.toRegex()).shouldBeTrue() }
-        val userId = createTestUserAndGetId(transaction, userTable, "usr71", originalPassword)
+        val currentPassword = "currentPassword"
+        val currentPasswordHash = byteArrayOf(1, 2, 3, 4)
+        val newPassword = "newPassword"
+        val confirmNewPassword = "newPasswordDifferent"
+        assert(newPassword != confirmNewPassword )
 
-        val newPassword = "5fkd^s91$-".also { it.matches(PASSWORD_REGEX.toRegex()).shouldBeTrue() }
-        val confirmNewPassword = "differ3nt".also { it.matches(PASSWORD_REGEX.toRegex()).shouldBeTrue() }
+        every { userTable.getUserById(userId) } returns User(
+            id = userId,
+            username = "user",
+            passwordHash = currentPasswordHash,
+            joinedInstant = Instant.now()
+        )
+        every { BCryptHelper.verifyPassword(currentPassword, currentPasswordHash) } returns true
 
-        changePassword(userId, originalPassword, newPassword, confirmNewPassword)
+        changePassword(userId, currentPassword, newPassword, confirmNewPassword)
             // New password not matching its confirmation should return UnconfirmedNewPasswordError.
             .shouldBeTypeOf<Result.UnconfirmedNewPasswordError>()
-
-        val latestUser = transaction { userTable.getUserById(userId) }
-
-        latestUser.shouldNotBeNull()
-        latestUser.username shouldBe "usr71"
-        BCryptHelper.verifyPassword(originalPassword, latestUser.passwordHash).shouldBeTrue()
     }
 
     @Test
     fun `Given an existing user, when changing password reusing the old password, NewPasswordNotNewError should be returned`() {
-        val originalPassword = "pass123".also { it.matches(PASSWORD_REGEX.toRegex()).shouldBeTrue() }
-        val userId = createTestUserAndGetId(transaction, userTable, "usr71", originalPassword)
+        val currentPassword = "currentPassword"
+        val currentPasswordHash = byteArrayOf(1, 2, 3, 4)
 
-        changePassword(userId, originalPassword, originalPassword, originalPassword)
+        every { userTable.getUserById(userId) } returns User(
+            id = userId,
+            username = "user",
+            passwordHash = currentPasswordHash,
+            joinedInstant = Instant.now()
+        )
+        every { BCryptHelper.verifyPassword(currentPassword, currentPasswordHash) } returns true
+
+        changePassword(userId, currentPassword, currentPassword, currentPassword)
             // New password being the same as the old one should return NewPasswordNotNewError.
             .shouldBeTypeOf<Result.NewPasswordNotNewError>()
-
-        val latestUser = transaction { userTable.getUserById(userId) }
-
-        latestUser.shouldNotBeNull()
-        latestUser.username shouldBe "usr71"
-        BCryptHelper.verifyPassword(originalPassword, latestUser.passwordHash).shouldBeTrue()
     }
 
     @Test
     fun `Given an existing user, when changing password using an invalid password, IllegalPasswordError should be returned`() {
-        val originalPassword = "pass123".also { it.matches(PASSWORD_REGEX.toRegex()).shouldBeTrue() }
-        val userId = createTestUserAndGetId(transaction, userTable, "usr71", originalPassword)
+        val currentPassword = "currentPassword"
+        val currentPasswordHash = byteArrayOf(1, 2, 3, 4)
+        val newPassword = "a"
+        // Invalid password
+        assert(!newPassword.matches(PASSWORD_REGEX.toRegex()))
 
-        val newInvalidPassword = "a".also { it.matches(PASSWORD_REGEX.toRegex()).shouldBeFalse() }
+        every { userTable.getUserById(userId) } returns User(
+            id = userId,
+            username = "user",
+            passwordHash = currentPasswordHash,
+            joinedInstant = Instant.now()
+        )
+        every { BCryptHelper.verifyPassword(currentPassword, currentPasswordHash) } returns true
 
-        changePassword(userId, originalPassword, newInvalidPassword, newInvalidPassword)
+        changePassword(userId, currentPassword, newPassword, newPassword)
             // New password being invalid should return IllegalPasswordError.
             .shouldBeTypeOf<Result.IllegalPasswordError>()
-
-        val latestUser = transaction { userTable.getUserById(userId) }
-
-        latestUser.shouldNotBeNull()
-        latestUser.username shouldBe "usr71"
-        BCryptHelper.verifyPassword(originalPassword, latestUser.passwordHash).shouldBeTrue()
     }
 }
