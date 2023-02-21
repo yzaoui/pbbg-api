3307cbe753fa4f5799924fced9cc9a26f4c73907
Yann Zaoui <contact@yannzaoui.com>
2023-02-21 14:08:10 -0500

    Create GetAvailableMines

---
+++ app/src/main/kotlin/domain/usecase/mine/GetAvailableMines.kt
@@ -0,0 +1,45 @@
+package com.bitwiserain.pbbg.app.domain.usecase.mine
+
+import com.bitwiserain.pbbg.app.db.Transaction
+import com.bitwiserain.pbbg.app.db.repository.UserStatsTable
+import com.bitwiserain.pbbg.app.domain.MiningExperienceManager
+import com.bitwiserain.pbbg.app.domain.model.mine.AvailableMines
+import com.bitwiserain.pbbg.app.domain.model.mine.MineType
+
+/**
+ * Gets the mines the user currently has access to.
+ */
+fun interface GetAvailableMines : (Int) -> AvailableMines {
+    /**
+     * @param userId The user's ID.
+     */
+    override fun invoke(userId: Int): AvailableMines
+}
+
+class GetAvailableMinesImpl(
+    private val transaction: Transaction,
+    private val userStatsTable: UserStatsTable,
+) : GetAvailableMines {
+
+    override fun invoke(userId: Int): AvailableMines {
+        val userMiningLevel = transaction {
+            userStatsTable.getUserStats(userId).miningExp
+        }.let { exp ->
+            MiningExperienceManager.getLevelProgress(exp)
+        }.level
+
+        val mines = mutableListOf<MineType>()
+        var nextUnlockLevel: Int? = null
+
+        for (mine in MineType.values()) {
+            if (userMiningLevel >= mine.minLevel) {
+                mines.add(mine)
+            } else {
+                nextUnlockLevel = mine.minLevel
+                break
+            }
+        }
+
+        return AvailableMines(mines, nextUnlockLevel)
+    }
+}
