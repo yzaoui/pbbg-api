99cea7588d202d06e9cba6884b687d0273f7a769
Yann Zaoui <contact@yannzaoui.com>
2023-02-21 20:58:11 -0500

    Create GenerateMineImplTest

---
+++ app/src/test/kotlin/domain/usecase/GenerateMineImplTest.kt
@@ -0,0 +1,50 @@
+package com.bitwiserain.pbbg.app.test.domain.usecase
+
+import com.bitwiserain.pbbg.app.db.repository.UserStatsTable
+import com.bitwiserain.pbbg.app.db.repository.mine.MineCellTable
+import com.bitwiserain.pbbg.app.db.repository.mine.MineSessionTable
+import com.bitwiserain.pbbg.app.domain.model.mine.MineType
+import com.bitwiserain.pbbg.app.domain.usecase.mine.GenerateMine.Result
+import com.bitwiserain.pbbg.app.domain.usecase.mine.GenerateMineImpl
+import com.bitwiserain.pbbg.app.test.db.TestTransaction
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class GenerateMineImplTest {
+
+    private val userId = 1234
+
+    private val mineCellTable: MineCellTable = mockk(relaxUnitFun = true)
+    private val mineSessionTable: MineSessionTable = mockk(relaxUnitFun = true)
+    private val userStatsTable: UserStatsTable = mockk(relaxUnitFun = true)
+
+    private val generateMine: GenerateMineImpl = GenerateMineImpl(TestTransaction, mineCellTable, mineSessionTable, userStatsTable)
+
+    @Test
+    @DisplayName("Given user is already in a mine, then should return AlreadyInMine")
+    fun alreadyInMine() {
+        // User is in a mine session
+        every { mineSessionTable.getSession(userId) } returns mockk()
+
+        val result = generateMine(userId, mineTypeId = 0, width = 0, height = 0)
+
+        result shouldBe Result.AlreadyInMine
+    }
+
+    @Test
+    @DisplayName("Given user is not in a mine, when requesting an invalid mine type, then should return InvalidMineTypeId")
+    fun invalidMineTypeId() {
+        // User is not in a mine session
+        every { mineSessionTable.getSession(userId) } returns null
+        val invalidMineType = 2000.also { check(it !in MineType.values().indices) }
+
+        val result = generateMine(userId, invalidMineType, width = 10, height = 10)
+
+        result shouldBe Result.InvalidMineTypeId
+    }
+
+    // TODO: More tests
+}
